43876d4af4e9ed65b29ffd447993e3020363e703
Yaroslav Nikolenko <yaroslavnikolenko@MacBook-Air-Yaroslav.local>
2024-08-02 14:10:35 +0300

    initial commit

---
+++ app/src/main/java/com/pfv/abzagencytesttask/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.pfv.abzagencytesttask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pfv.abzagencytesttask.ui.theme.AbzagencyTestTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AbzagencyTestTaskTheme {
+
+                enableEdgeToEdge()
+
+            }
+        }
+    }
+}
