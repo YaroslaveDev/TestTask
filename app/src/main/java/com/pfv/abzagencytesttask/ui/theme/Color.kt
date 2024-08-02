25ee8f9f4695e69f3fb1d004a10b423a52e56e65
Yaroslav Nikolenko <yaroslavnikolenko@MacBook-Air-Yaroslav.local>
2024-08-02 14:54:08 +0300

    feature: setup colors & styles

--- app/src/main/java/com/pfv/abzagencytesttask/ui/theme/Color.kt
+++ app/src/main/java/com/pfv/abzagencytesttask/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.pfv.abzagencytesttask.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFFF4E041)
+val OnPrimary = Color(0xFFFFC700)
+val Secondary = Color(0xFF00BDD3)
+val OnSecondary = Color(0x1A00BDD3)
+val Surface = Color(0xFFFFFFFF)
+val Error = Color(0xFFCB3D40)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Grey = Color(0xFFDEDEDE)
+val Black_87 = Color(0xDE000000)
+val Black_60 = Color(0x99000000)
+val Black_48 = Color(0x7A000000)
